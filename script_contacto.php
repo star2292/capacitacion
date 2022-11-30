c55f7f7bc2add8be7d5f69c93a8190457e89add6
ivanreyes <ireyess@nafin.gob.mx>
2022-11-29 19:16:13 -0600

    se agregan cambios

--- script_contacto.php
+++ script_contacto.php
@@ -9,9 +9,9 @@
 	if(preg_match("/^[A-Za-z0-9][A-Za-z0-9_.-]+[A-za-z]*@[A-Za-z0-9_-]+\.[A-Za-z0-9_.-]+[A-za-z]$/", $correo) && preg_match("/^[a-zA-Z]+$/", $name)  && 
 		preg_match("/^[0-9]+$/", $phone) && preg_match("/[a-zA-Z.:;]+$/", $mensaje)){
 		
-		if (file_exists("./contacto.csv")) {
+		if (file_exists("./csv/contacto.csv")) {
 			if(strlen($correo) >= 15 && strlen($correo) <= 35){
-				$fp = fopen('./contacto.csv', 'a');
+				$fp = fopen('./csv/contacto.csv', 'a');
 				fwrite($fp, "fecha:" . date("y:m:d") . ", hora:" . date("H:i:s") . ", correo:" . $correo . ", nombre:" . $name . ", telefono:" . $phone . ", mensaje:" . $mensaje . ",");
 				fwrite($fp, "\n");
 				fclose($fp);
@@ -20,7 +20,7 @@
 			
 			#file_put_contents('contacto.json', json_encode(array(date("m.d.y"), date("H:i:s"), $correo), JSON_FORCE_OBJECT, JSON_UNESCAPED_UNICODE) . ",", FILE_APPEND);
 		} else {
-			$fp = fopen('./contacto.csv', 'w');
+			$fp = fopen('./csv/contacto.csv', 'w');
 			fwrite($fp, "fecha:" . date("y:m:d") . ", hora:" . date("H:i:s") . ", correo:" . $correo . ", nombre:" . $name . ", telefono:" . $phone . ", mensaje:" . $mensaje . ",");
 				fwrite($fp, "\n");
 			fclose($fp);
--- script_suscripcion.php
+++ script_suscripcion.php
@@ -3,19 +3,18 @@
 	
 	$correo = $_POST['correo'];
 	
-	if(preg_match("/^[A-Za-z0-9][A-Za-z0-9_.-]+[A-za-z]*@[A-Za-z0-9_-]+\.[A-Za-z0-9_.-]+[A-za-z]$/", $correo)){
-		if (file_exists("./suscripcion.csv")) {
+	if(isset($correo) && preg_match("/^[A-Za-z0-9][A-Za-z0-9_.-]+[A-za-z]*@[A-Za-z0-9_-]+\.[A-Za-z0-9_.-]+[A-za-z]$/", $correo)){
+		if (file_exists("./csv/suscripcion.csv")) {
 			if(strlen($correo) >= 15 && strlen($correo) <= 35){
-				$fp = fopen('./suscripcion.csv', 'a');
+				$fp = fopen('./csv/suscripcion.csv', 'a');
 				fwrite($fp, "fecha:" . date("y:m:d") . ", hora:" . date("H:i:s") . ", correo:" . $correo . ",");
 				fwrite($fp, "\n");
 				fclose($fp);
 		
 			}
 			
-			#file_put_contents('suscripcion.json', json_encode(array(date("m.d.y"), date("H:i:s"), $correo), JSON_FORCE_OBJECT, JSON_UNESCAPED_UNICODE) . ",", FILE_APPEND);
 		} else {
-			$fp = fopen('./suscripcion.csv', 'w');
+			$fp = fopen('./csv/suscripcion.csv', 'w');
 			fwrite($fp, "fecha:" . date("y:m:d") . ", hora:" . date("H:i:s") . ", correo:" . $correo . ",");
 			fwrite($fp, "\n");
 			fclose($fp);
